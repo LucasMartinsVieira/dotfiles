0768e247d51172c8c6d9fd587ee8e2ba14b55c27
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-09-02 18:03:17 -0300

    Fix: removing personal info in user_variables.lua

--- .config/awesome/user_variables.lua
+++ .config/awesome/user_variables.lua
@@ -2,11 +2,11 @@ return {
   widget = {
     weather = {
       -- API Key
-        key = "d66e9508671053721051099df3ab79cf",
+        key = "",
       -- Cordinates
         cordinates = {
-          "-23.6639", --lat
-          "-46.5383", --lon
+          "", --lat
+          "", --lon
       },
     },
   },
