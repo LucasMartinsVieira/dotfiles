34c7d40a9b4c40e3ec423b8136f9b0de54586cf4
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-09-05 15:51:39 -0300

    add a solarized dark and monokai pro themes

---
+++ .config/awesome/theme/monokai_pro.lua
@@ -0,0 +1,14 @@
+-- Monokai Pro Colors
+local colors = {}
+
+-- Bg Colors 
+colors.bg_color    = "#403e41"
+colors.bg_focus    = "#423f42"
+colors.bg_urgent   = "#FD6883"
+colors.bg_minimize = "#2D2A2E"
+colors.bg_systray  = "#2D2A2E"
+ 
+-- Fg Colors
+colors.fg_color   = "#FCFCFA"
+
+return colors
---
+++ .config/awesome/theme/solarized_dark.lua
@@ -0,0 +1,14 @@
+-- Solarized Dark Colors
+local colors = {}
+
+-- Bg Colors 
+colors.bg_color    = "#002b36"
+colors.bg_focus    = "#073642"
+colors.bg_urgent   = "#dc322f"
+colors.bg_minimize = "#002b36"
+colors.bg_systray  = "#002b36"
+ 
+-- Fg Colors
+colors.fg_color   = "#839496"
+
+return colors
