8352b604a3b05736c24e9c929292299be0b5d0f5
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-09-06 17:23:30 -0300

    add alpha.lua

---
+++ .config/nvim/lua/user/alpha.lua
@@ -0,0 +1,45 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+  return
+end
+
+local dashboard = require "alpha.themes.dashboard"
+
+local function button(sc, txt, keybind, keybind_opts)
+  local b = dashboard.button(sc, txt, keybind, keybind_opts)
+  b.opts.hl_shortcut = "Macro"
+  return b
+end
+
+dashboard.section.header.val = {
+[[  ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗]],
+[[  ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║]],
+[[  ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║]],
+[[  ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║]],
+[[  ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║]],
+[[  ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝]],
+}
+dashboard.section.buttons.val = {
+        button("f", "  Find file", "<CMD>FZF<CR>"),
+        button("e", "  New file", "<cmd>ene!<CR>"),
+        button("w", "  Find word", "<CMD>Telescope live_grep<CR>"),
+        button("r", "  Recent Files", "<CMD>Telescope oldfiles<CR>"),
+        button("p", "  Recent Projects", "<CMD>Telescope projects<CR>"),
+        button("m", "  Vim Wiki", ":VimwikiIndex<CR>"),
+        button("c", "  Config", ":e ~/.config/nvim/init.lua<CR>"),
+        button("u", "  Update", ":PackerSync<CR>"),
+        button("q", "  Quit", "e :qa<CR>"),
+}
+local function footer()
+  return "https://github.com/LucasMartinsVieira"
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Macro"
+dashboard.section.footer.opts.hl = "Type"
+
+dashboard.opts.opts.noautocmd = true
+-- vim.cmd([[autocmd User AlphaReady echo 'ready']])
+alpha.setup(dashboard.opts)
