2b8ed05de75dcbbe1251d944b1a1f22ca4cf84f4
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-09-30 19:54:31 -0300

    Fix: Making working with Neovim 0.8

--- .config/nvim/lua/user/lsp/handlers.lua
+++ .config/nvim/lua/user/lsp/handlers.lua
@@ -47,7 +47,7 @@ end
 
 local function lsp_highlight_document(client)
 	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
+	if client.server_capabilities.document_highlight then
 		vim.api.nvim_exec(
 			[[
       augroup lsp_document_highlight
@@ -86,8 +86,8 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" or client.name == "rust_analyzer" or client.name == "dartls" or client.name == "sumneko_lua" then
-		client.resolved_capabilities.document_formatting = false
-		client.resolved_capabilities.document_range_formatting = false
+		client.server_capabilities.document_formatting = false
+		client.server_capabilities.document_range_formatting = false
 	end
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
