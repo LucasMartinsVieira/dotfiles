eb0992618888eb2844a35ffc1817d804a4b9c0fb
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-10-06 17:58:30 -0300

    Add horizon theme

---
+++ .config/awesome/theme/horizon.lua
@@ -0,0 +1,14 @@
+-- Horizon Colors
+local colors = {}
+
+-- Bg Colors 
+colors.bg_color    = "#1d1f27"
+colors.bg_focus    = "#afb0af"
+colors.bg_urgent   = "#e95678"
+colors.bg_minimize = "#ff2222"
+colors.bg_systray  = "#4a4b52"
+ 
+-- Fg Colors
+colors.fg_color   = "#8b8d8f"
+
+return colors
