cf2f3f97702251028f2609d9039e184907f0a3a9
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-10-12 18:52:23 -0300

    Minor changes

--- .config/nvim/lua/user/treesitter.lua
+++ .config/nvim/lua/user/treesitter.lua
@@ -6,8 +6,10 @@ end
 configs.setup {
   ensure_installed = "all",
   sync_install = false,
+  ignore_install = { "" },
   highlight = {
     enable = true,
+    disable = {},
     additional_vim_regex_highlighting = false,
   },
   indent = {
