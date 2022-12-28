a6b1d69fb3374e37139377b4f96c228fe043838a
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2022-12-28 18:05:51 -0300

    Remove titlebar(unsed) and colorized layouts

--- .config/awesome/theme/theme.lua
+++ .config/awesome/theme/theme.lua
@@ -14,8 +14,10 @@ local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 local gfs = require("gears.filesystem")
 local themes_path = gfs.get_themes_dir()
-local icons_dir = require("gears").filesystem.get_configuration_dir() .."/icons/"
+local gcolor = require("gears.color")
+local icons_dir = require("gears").filesystem.get_configuration_dir() .. "/theme/assets/"
 local theme = {}
+
 
 local colors = require('theme.nightfly') -- This has to be in single quotes for rofi-colorscheme to work
 
@@ -32,10 +34,13 @@ theme.minimize_bg = colors.bg_minimize
 theme.systray_bg  = colors.bg_systray
 theme.color_fg    = colors.fg_color
 
--- Taglist colors
+-- Taglist
 theme.taglist_fg_focus = theme.yellow
 theme.taglist_fg_occupied = theme.blue
 theme.taglist_bg_focus = colors.bg_color
+-- Tasklist
+theme.tasklist_bg_focus = colors.bg_color
+theme.tasklist_disable_task_name = true
 
 -- Font
 theme.font = "JetBrains Mono 9"
@@ -51,8 +56,6 @@ theme.fg_normal   = theme.color_fg
 theme.fg_focus    = theme.color_fg
 theme.fg_urgent   = theme.color_fg
 theme.fg_minimize = theme.color_fg
-
-theme.tasklist_bg_focus = colors.bg_color
 
 -- Gaps
 theme.useless_gap   = dpi(4)
@@ -74,7 +77,7 @@ theme.notification_shape = function(cr, width, height)
 end
 
 -- Menu
-theme.submenu = "  "
+theme.submenu = "    "
 theme.menu_height           = dpi(25)
 theme.menu_width            = dpi(180)
 theme.menu_border_width	    = dpi(0)
@@ -90,50 +93,24 @@ theme.hotkeys_font              = theme.font
 theme.hotkeys_description_font  = theme.font
 theme.hotkeys_group_margin      = dpi(10)
 
--- Define the image to load
-theme.titlebar_close_button_normal = themes_path .. "default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = themes_path .. "default/titlebar/close_focus.png"
-
-theme.titlebar_minimize_button_normal = themes_path .. "default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path .. "default/titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = themes_path .. "default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path .. "default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active   = themes_path .. "default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active    = themes_path .. "default/titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = themes_path .. "default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path .. "default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active   = themes_path .. "default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active    = themes_path .. "default/titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = themes_path .. "default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path .. "default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active   = themes_path .. "default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active    = themes_path .. "default/titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = themes_path .. "default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path .. "default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active   = themes_path .. "default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active    = themes_path .. "default/titlebar/maximized_focus_active.png"
-
 -- You can use your own layout icons like this:
-theme.layout_fairh      = themes_path .. "default/layouts/fairhw.png"
-theme.layout_fairv      = themes_path .. "default/layouts/fairvw.png"
-theme.layout_floating   = themes_path .. "default/layouts/floatingw.png"
-theme.layout_magnifier  = themes_path .. "default/layouts/magnifierw.png"
-theme.layout_max        = themes_path .. "default/layouts/maxw.png"
+theme.layout_floating       =  gcolor.recolor_image(icons_dir .. "layout/floating.png", theme.green)
+theme.layout_max       =  gcolor.recolor_image(icons_dir .. "layout/max.png", theme.yellow)
+theme.layout_tile       =  gcolor.recolor_image(icons_dir .. "layout/tile.png", theme.blue)
 theme.layout_fullscreen = themes_path .. "default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path .. "default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path .. "default/layouts/tileleftw.png"
-theme.layout_tile       = themes_path .. "default/layouts/tilew.png"
-theme.layout_tiletop    = themes_path .. "default/layouts/tiletopw.png"
-theme.layout_spiral     = themes_path .. "default/layouts/spiralw.png"
-theme.layout_dwindle    = themes_path .. "default/layouts/dwindlew.png"
-theme.layout_cornernw   = themes_path .. "default/layouts/cornernww.png"
-theme.layout_cornerne   = themes_path .. "default/layouts/cornernew.png"
-theme.layout_cornersw   = themes_path .. "default/layouts/cornersww.png"
-theme.layout_cornerse   = themes_path .. "default/layouts/cornersew.png"
+
+--theme.layout_fairh      = themes_path .. "default/layouts/fairhw.png"
+--theme.layout_fairv      = themes_path .. "default/layouts/fairvw.png"
+--theme.layout_magnifier  = themes_path .. "default/layouts/magnifierw.png"
+--theme.layout_tilebottom = themes_path .. "default/layouts/tilebottomw.png"
+--theme.layout_tileleft   = themes_path .. "default/layouts/tileleftw.png"
+--theme.layout_tiletop    = themes_path .. "default/layouts/tiletopw.png"
+--theme.layout_spiral     = themes_path .. "default/layouts/spiralw.png"
+--theme.layout_dwindle    = themes_path .. "default/layouts/dwindlew.png"
+--theme.layout_cornernw   = themes_path .. "default/layouts/cornernww.png"
+--theme.layout_cornerne   = themes_path .. "default/layouts/cornernew.png"
+--theme.layout_cornersw   = themes_path .. "default/layouts/cornersww.png"
+--theme.layout_cornerse   = themes_path .. "default/layouts/cornersew.png"
 -- Generate Awesome icon:
 theme.awesome_icon      = theme_assets.awesome_icon(
   theme.menu_height, theme.bg_focus, theme.fg_focus
