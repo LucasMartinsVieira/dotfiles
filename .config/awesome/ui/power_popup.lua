ab5672d28bb2cfbcae9f539efee36f1602d92135
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-01-10 08:54:51 -0300

    Merging the two powermenus

--- .config/awesome/ui/power_popup.lua
+++ .config/awesome/ui/power_popup.lua
@@ -5,8 +5,9 @@ local gears = require("gears")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
--- Button size
-local button_size = dpi(22)
+-- Buttons
+local button_bg = theme.bg_focus
+local button_size = dpi(42)
 
 -- Powermenu Icons
 local power_icon = theme.power
@@ -73,13 +74,59 @@ local reboot = create_button(reboot_icon, reboot_command)
 local logout = create_button(logout_icon, logout_command)
 local suspend = create_button(suspend_icon, suspend_command)
 
+-- Profile pic
+local pic = wibox.widget({
+  nil,
+  {
+    {
+      image = theme.pfp,
+      resize = true,
+      clip_shape = gears.shape.circle,
+      widget = wibox.widget.imagebox,
+    },
+    border_width = dpi(3),
+    border_color = theme.blue,
+    bg = theme.blue,
+    forced_height = dpi(120),
+    forced_width = dpi(120),
+    shape = gears.shape.circle,
+    widget = wibox.container.background,
+  },
+  nil,
+  expand = "none",
+  layout = wibox.layout.align.horizontal,
+})
+
+-- Get username
+local user = os.getenv("USER")
+
+local username = wibox.widget.textbox(user:sub(1, 1):upper() .. user:sub(2))
+username.font = theme.font .. " 18"
+username.align = "center"
+username.valign = "center"
+
+-- Uptime
+local uptime = wibox.widget({
+  text = "up 0 minutes",
+  font = theme.font .. " 12",
+  align = "center",
+  valign = "center",
+  widget = wibox.widget.textbox,
+})
+
+awful.widget.watch("uptime -p", 60, function(_, stdout)
+  -- Remove trailing whitespaces
+  local out = stdout:gsub("^%s*(.-)%s*$", "%1") --:gsub(", ", ",\n")
+  uptime.text = out
+end)
+
 p = wibox({
   visible = false,
   ontop = true,
   bg = theme.bg_normal,
   fg = theme.blue,
-  height = dpi(35),
-  width = dpi(160),
+  height = dpi(300),
+  width = dpi(400),
   shape = function(cr, width, height)
     gears.shape.rounded_rect(cr, width, height, 12)
   end,
@@ -99,7 +146,6 @@ p:buttons(gears.table.join(
   end)
 ))
 
--- Function to show the popup
 function power_popup_show()
   if p.visible == false then
     p.visible = true
@@ -115,11 +161,18 @@ p:setup({
     nil,
     {
       {
+        pic,
+        username,
+        uptime,
+        spacing = dpi(10),
+        layout = wibox.layout.fixed.vertical,
+      },
+      {
         power,
         reboot,
         logout,
         suspend,
-        spacing = dpi(14),
+        spacing = dpi(40),
         layout = wibox.layout.fixed.horizontal,
       },
       spacing = dpi(30),
