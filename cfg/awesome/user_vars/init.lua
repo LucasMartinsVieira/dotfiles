b1423d679640b83d7373d7181417ce68bb2e1c01
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-02-27 19:20:56 -0300

    Awesome: Oops

---
+++ cfg/awesome/user_vars/init.lua
@@ -0,0 +1,16 @@
+return {
+  weather = {
+    --- API Key
+    key = "",
+    --- Coordinates
+    coordinates = {
+      "", --- lat
+      "", --- lon
+    },
+  },
+  user = {
+    -- Your Wallpapers directory
+    wallpapers_dir = "",
+  },
+}
+
