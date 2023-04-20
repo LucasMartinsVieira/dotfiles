0f380643d65cd5428a009756afe5f335175222bc
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-04-20 08:13:49 -0300

    Nvim: Fixed bufferline plugin (finally)

--- cfg/nvim/lua/plugins/bufferline.lua
+++ cfg/nvim/lua/plugins/bufferline.lua
@@ -1,24 +1,22 @@
 local icons = require("user.icons")
 
 return {
-  {
-    "akinsho/bufferline.nvim",
-    version = "v3.*",
-    enabled = true,
-    event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers", },
-    },
-
-    opts = {
+  "akinsho/bufferline.nvim",
+  version = "v3.*",
+  enabled = true,
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle pin", },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+  },
+  config = function()
+    require("bufferline").setup({
       options = {
-        numbers = "none",                -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
-        close_command = "bdelete! %d",   -- can be a string | function, see "Mouse actions"
-        right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-        left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
-        middle_mouse_command = nil,      -- can be a string | function, see "Mouse actions"
-        -- indicator_icon = icons.ui.Separator,
+        numbers = "none",                      -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+        close_command = "bdelete! %d",         -- can be a string | function, see "Mouse actions"
+        right_mouse_command = "bdelete! %d",   -- can be a string | function, see "Mouse actions"
+        left_mouse_command = "buffer %d",      -- can be a string | function, see "Mouse actions"
+        middle_mouse_command = nil,            -- can be a string | function, see "Mouse actions"
         indicator = {
           icon = icons.ui.Separator,
           style = 'icon',
@@ -29,9 +27,9 @@ return {
         left_trunc_marker = "",
         right_trunc_marker = "",
         max_name_length = 30,
-        max_prefix_length = 30, -- prefix used when a buffer is de-duplicated
+        max_prefix_length = 30,     -- prefix used when a buffer is de-duplicated
         tab_size = 21,
-        diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
+        diagnostics = "nvim_lsp",   -- | "nvim_lsp" | "coc",
         diagnostics_update_in_insert = false,
         offsets = {
           {
@@ -45,93 +43,11 @@ return {
         show_buffer_close_icons = true,
         show_close_icon = true,
         show_tab_indicators = true,
-        persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-        separator_style = "thin", -- | "thick" | "thin" | { 'any', 'any' },
+        persist_buffer_sort = true,   -- whether or not custom sorted buffers should persist
+        separator_style = "thin",     -- | "thick" | "thin" | { 'any', 'any' },
         enforce_regular_tabs = true,
         always_show_bufferline = false,
       },
-      highlights = {
-        fill = {
-          fg = { attribute = "fg", highlight = "#ff0000" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        background = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        buffer_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        close_button = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        close_button_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        tab_selected = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        tab = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        tab_close = {
-          fg = { attribute = "fg", highlight = "TabLineSel" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        duplicate_selected = {
-          fg = { attribute = "fg", highlight = "TabLineSel" },
-          bg = { attribute = "bg", highlight = "TabLineSel" },
-          underline = true,
-          undercurl = true,
-          italic = true,
-        },
-        duplicate_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-          underline = true,
-          undercurl = true,
-          italic = true,
-        },
-        duplicate = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-          underline = true,
-          undercurl = true,
-          italic = true,
-        },
-        modified = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        modified_selected = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        modified_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        separator = {
-          fg = { attribute = "bg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        separator_selected = {
-          fg = { attribute = "bg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        indicator_selected = {
-          fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("bufferline").setup(opts)
-    end,
-  },
+    })
+  end,
 }
