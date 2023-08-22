66ac2cdbc1dab757bd28590e1418a69c3741f40a
LucasMartinsVieira <lucasmartvieira03@outlook.com>
2023-08-22 11:04:00 -0300

    Modularized hyprland config

--- cfg/nvim/lua/plugins/colorizer.lua
+++ cfg/nvim/lua/plugins/colorizer.lua
@@ -16,11 +16,11 @@ return {
         "lua",
       },
       user_default_options = {
-        -- RGB = true, -- #RGB hex codes
-        -- RRGGBB = true, -- #RRGGBB hex codes
-        -- names = false, -- "Name" codes like Blue or blue
-        -- RRGGBBAA = false, -- #RRGGBBAA hex codes
-        -- AARRGGBB = false, -- 0xAARRGGBB hex codes
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        names = false, -- "Name" codes like Blue or blue
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
         rgb_fn = true, -- CSS rgb() and rgba() functions
         -- hsl_fn = true, -- CSS hsl() and hsla() functions
         -- css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
