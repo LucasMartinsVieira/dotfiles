b1c997d9035f8b98aeaa8fa5c6460e108be5e332
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2025-01-18 14:48:11 -0300

    feat(nvim): add dadbob plugin

--- cfg/nvim/lua/plugins/blink.lua
+++ cfg/nvim/lua/plugins/blink.lua
@@ -2,22 +2,19 @@ return {
   "saghen/blink.cmp",
   enabled = false,
   -- optional: provides snippets for the snippet source
-  dependencies = "rafamadriz/friendly-snippets",
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+    "saghen/blink.compat",
+    { "L3MON4D3/LuaSnip", version = "2.*", build = "make install_jsregexp" },
+    -- CMP completions
+    { "hrsh7th/cmp-emoji", event = "InsertEnter" },
+  },
 
-  -- use a release tag to download pre-built binaries
   version = "*",
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' for mappings similar to built-in completion
-    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-    -- See the full "keymap" documentation for information on defining your own keymap.
     keymap = {
       preset = "none",
 
@@ -36,21 +33,29 @@ return {
     },
 
     appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- Will be removed in a future release
       use_nvim_cmp_as_default = true,
-      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
+    },
+
+    snippets = {
+      preset = "luasnip",
     },
 
     signature = { enabled = true },
 
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "snippets", "buffer", "emoji", "crates" },
+
+      providers = {
+        emoji = {
+          name = "emoji",
+          module = "blink.compat.source",
+        },
+        crates = {
+          name = "crates",
+          module = "blink.compat.source",
+        },
+      },
     },
   },
   opts_extend = { "sources.default" },
--- cfg/nvim/lua/plugins/treesitter/context.lua
+++ cfg/nvim/lua/plugins/treesitter/context.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-context",
+  enabled = false,
   config = function()
     require("treesitter-context").setup({
       enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
