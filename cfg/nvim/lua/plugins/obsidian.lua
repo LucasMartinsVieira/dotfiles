7514b1aa0e0fc787e6bc396873a1e905851e51fd
Lucas Martins Vieira <lucasmartvieira03@outlook.com>
2025-03-16 19:58:11 -0300

    chore(nvim): add ft markdown

--- cfg/nvim/lua/plugins/obsidian.lua
+++ cfg/nvim/lua/plugins/obsidian.lua
@@ -72,6 +72,7 @@ return {
     {
       "MeanderingProgrammer/render-markdown.nvim",
       dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+      ft = { "markdown" },
       config = function()
         require("render-markdown").setup({
           heading = {
